47a88b15fd447d9a4b2681996dc005b2c08313d2
affan9431 <123025121+affan9431@users.noreply.github.com>
2023-09-30 16:21:39 +0530

    Add files via upload

---
+++ First websites/web.js
@@ -0,0 +1,37 @@
+$("#submit").click(function () {
+    alert('YOU LOG IN TO THIS PAGE');
+})
+
+$("#submit2").click(function () {
+    alert("YOU CRAETE A NEW ACCOUNT");
+})
+
+
+$(".view").click(function () {
+    location.href = "http://127.0.0.1:5500/about.html";
+});
+
+$("#log").click(function () {
+    console.log("Clicked");
+})
+
+$("#Keshav").click(function () {
+    alert("WAIT FOR A MOMENT...");
+})
+
+$("#Aayush").click(function () {
+    alert("WAIT FOR A MOMENT...");
+})
+
+$("#Abhishek").click(function () {
+    alert("WAIT FOR A MOMENT...");
+})
+
+$("#search").click(function(){
+    alert("Clicked");
+})
+
+
+
+
+
