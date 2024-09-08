03c1ee018edab24385d21bfc6ce559f7180ba8ad
affan9431 <123025121+affan9431@users.noreply.github.com>
2024-09-08 22:10:22 +0530

    Add files via upload

---
+++ starter/index.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('../module/replaceTemplate');
+
+/////////////////////////////
+// FILE
+
+// Blocking synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt",  "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File  outputted to ./txt/output.txt");
+
+// Non-Blocking Asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile(`./txt/final.txt`, `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("File has been written 🫡");
+//       });
+//     });
+//   });
+// });
+
+// console.log("Will read file!");
+
+//////////////////////////////
+// SERVER
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+  const pathName = req.url;
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+
+    const cardsHtml = dataObj
+      .map((el) => {
+        return replaceTemplate(tempCard, el);
+      })
+      .join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+  }
+
+  // Product page
+  else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+  // API
+  else if (pathname === '/api') {
+    res.writeHead(200, { 'Content-type': 'application.json' });
+    res.end(data);
+  }
+  // Not found
+  else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end(`<h1>PAGE NOT FOUND</h1>`);
+  }
+});
+
+// Optional arguments: 127.0.0.1 => It's basically means  localhost
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port 8000');
+});
